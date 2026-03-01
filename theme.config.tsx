d7678bb1ce8b7d401772a38da78e7d53ade48385
nana <nanafromjeju@gmail.com>
2026-03-01 20:40:20 +0900

    chore: 프로젝트 기본 세팅

---
+++ theme.config.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
+
+const config: DocsThemeConfig = {
+  logo: <span>Inbom DS</span>,
+  project: {
+    link: 'https://github.com/nanafromjeju/inbom-ds',
+  },
+};
+
+export default config;
