80d87bee31a08b621ecab816d40ef0ac7bf59989
nana <nanafromjeju@gmail.com>
2026-03-04 15:09:15 +0900

    docs: README.md 작성

---
+++ components/Motion/FadeIn/FadeIn.tsx
@@ -0,0 +1,15 @@
+import { motion } from 'framer-motion';
+
+const FadeIn = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 16 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default FadeIn;
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
+import FadeIn from '@/components/Motion/FadeIn';
 
-const config: DocsThemeConfig = {
+export default {
   logo: <span>Inbom DS</span>,
-  project: {
-    link: 'https://github.com/nanafromjeju/inbom-ds',
+  head: (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="title" content="Inbom Design System" />
+    </>
+  ),
+  components: {
+    FadeIn,
   },
 };
-
-export default config;
